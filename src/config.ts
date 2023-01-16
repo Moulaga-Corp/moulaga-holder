67317db3ac2c7a42705a4d0daebf5b9e0b43eb01
Paolo Manaois <paolo.manaois@yahoo.fr>
2023-01-16 10:32:50 +0100

    updated scripts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { get } from "env-var";
 
 const CONFIG = {
-	WALLET_PRIVATE_KEY: get("WALET_PRIVATE_KEY").required().asString(),
+	WALLET_PRIVATE_KEY: get("WALLET_PRIVATE_KEY").required().asString(),
 	PORT: get("PORT").default(3000).asPortNumber(),
 } as const;
 
